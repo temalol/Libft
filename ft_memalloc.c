eb4eca3818ba3d1beebcd5fa88de1161c6b25195
Nickie Mustache <nmustach@ox-d5.msk.21-school.ru>
2020-11-22 15:56:56 +0300

    is_number

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memalloc.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nmustach <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: nmustach <nmustach@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/13 19:47:56 by nmustach          #+#    #+#             */
-/*   Updated: 2019/09/13 19:49:18 by nmustach         ###   ########.fr       */
+/*   Updated: 2020/11/22 15:56:07 by nmustach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	*ft_memalloc(size_t size)
 
 	ptr = (unsigned char*)malloc(size * sizeof(unsigned char));
 	if (ptr == NULL)
-		return (0);
+		return (NULL);
 	ft_memset(ptr, 0, size);
 	return (ptr);
 }
