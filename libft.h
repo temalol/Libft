452a5a578936683b4464b62056b928bb3347efcf
Nickie Mustache <nmustach@ox-d5.msk.21-school.ru>
2020-11-22 15:54:06 +0300

    is_number

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: nmustach <nmustach@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/12 19:27:53 by nmustach          #+#    #+#             */
-/*   Updated: 2020/11/22 15:33:14 by nmustach         ###   ########.fr       */
+/*   Updated: 2020/11/22 15:53:34 by nmustach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,4 +87,5 @@ void			ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void			ft_lstadd(t_list **alst, t_list *new);
 void			ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 void			ft_strrev(char *input);
+int				ft_is_number(char *input);
 #endif
